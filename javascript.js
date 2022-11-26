d35422445c4e5cb2eb9a2cacc6f0853b125f6f04
TofuMing <henrygao48@gmail.com>
2022-11-26 18:01:10 +1300

    Update playRound function

--- javascript.js
+++ javascript.js
@@ -11,27 +11,29 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let result;
     playerSelection = toLowerCase(playerSelection);
     switch (playerSelection) {
         case "rock":
             switch (computerSelection) {
-                case "Rock": console.log("You Drew! Rock has no effect on Rock");
-                case "Paper": console.log("You Lose! Paper beats Rock");
-                case "Scissors": console.log("You Win! Rock beats Scissors")
+                case "Rock": result = "You Drew! Rock has no effect on Rock";
+                case "Paper": result = "You Lose! Paper beats Rock";
+                case "Scissors": result = "You Win! Rock beats Scissors";
             }
         case "paper":
             switch (computerSelection) {
-                case "Rock": console.log("You Won! Paper beats Rock");
-                case "Paper": console.log("You Drew! Paper has no effect on Paper");
-                case "Scissors": console.log("You Lose! Scissors beats Paper")
+                case "Rock": result = "You Won! Paper beats Rock";
+                case "Paper": result = "You Drew! Paper has no effect on Paper";
+                case "Scissors": result = "You Lose! Scissors beats Paper";
             }
         case "scissors":
             switch (computerSelection) {
-                case "Rock": console.log("You Lose! Rock beats Scissors");
-                case "Paper": console.log("You Won! Scissors beats Paper");
-                case "Scissors": console.log("You Drew! Scissors has no effect on Scissors")
+                case "Rock": result = "You Lose! Rock beats Scissors";
+                case "Paper": result = "You Won! Scissors beats Paper";
+                case "Scissors": result = "You Drew! Scissors has no effect on Scissors";
             }
         default:
-            console.log("Please enter a Rock, Paper or Scissors")
+            result = "Please enter a Rock, Paper or Scissors";
     }
+    return Result;
 }
